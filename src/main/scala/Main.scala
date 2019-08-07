eeae06ec67061b199932d78a327c3baf3d770edb
Daniel Smith <39262851+dsmith2708@users.noreply.github.com>
2019-08-07 10:50:02 +0100

    code now allows numbers to be seperated by new line

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,7 +6,12 @@ object Main {
   def add(numbers: String): Int = {
     numbers match {
       case "" => 0
+      case x if(x.contains(System.lineSeparator)) => {
+        println("contains \\n")
+        numbers.split(System.lineSeparator).map(_.toInt).sum
+      }
       case _ => {
+        println("does not contain \\n")
         numbers.split(",").map(_.toInt).sum
       }
     }
