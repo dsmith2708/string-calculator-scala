c5863a812687ba3e97815588909afcb61c8f88e4
Daniel Smith <39262851+dsmith2708@users.noreply.github.com>
2019-08-07 10:37:19 +0100

    refactored all tests

--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -5,15 +5,20 @@ class MainSpec extends WordSpec with Matchers {
   "The add function" when {
     "called with an empty string" should {
       "return 0" in {
-        Main.add("")
+        Main.add("") shouldBe 0
       }
     }
-    "called with a string of numbers separated by commas" should {
+    "called with a string of 3 numbers separated by commas" should {
       "return the sum of 1,2,3" in {
-        Main.add("1,2,3")
+        Main.add("1,2,3") shouldBe 6
       }
       "return the sum of 3,4,5" in {
-        Main.add("4,5,6")
+        Main.add("4,5,6") shouldBe 15
+      }
+    }
+    "called with a string of an unknown amount of numbers separated by commas" should {
+      "return the sum of 1,2,3,4,5,6" in {
+        Main.add("1,2,3,4,5,6") shouldBe 21
       }
     }
   }
