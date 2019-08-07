31643fa56d388054f4674acf8c8e83edbd540b18
Daniel Smith <39262851+dsmith2708@users.noreply.github.com>
2019-08-07 11:50:59 +0100

    refactored code to remove syntax warnings

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,13 +6,10 @@ object Main {
   def add(numbers: String): Int = {
     numbers match {
       case "" => 0
-      case x if(x.take(2) == "\\\\") => {
+      case x if x.take(2) == "\\\\" =>
         val delimiter: String = x.charAt(2).toString
         x.drop(4).split(delimiter).map(_.toInt).sum
-      }
-      case _ => {
-        numbers.split(s"[,${System.lineSeparator()}]").map(_.toInt).sum
-      }
+      case _ => numbers.split(s"[,${System.lineSeparator()}]").map(_.toInt).sum
     }
   }
 }
